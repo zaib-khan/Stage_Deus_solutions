3b678dce8de0cd2e060e7c15041fe6f632ab5c35
zaib khan <zaib16khan@gmail.com>
2022-09-26 08:43:48 +0200

    zaib

--- drag_and_drop_all_tests/src/app/container-in-container/container-in-container.component.ts
+++ drag_and_drop_all_tests/src/app/container-in-container/container-in-container.component.ts
@@ -1,4 +1,4 @@
-import { CdkDragDrop } from '@angular/cdk/drag-drop';
+import { CdkDragDrop, CdkDragEnter} from '@angular/cdk/drag-drop';
 import { Component, OnInit,Renderer2,ViewChild,ElementRef } from '@angular/core';
 
 @Component({
@@ -8,8 +8,8 @@ import { Component, OnInit,Renderer2,ViewChild,ElementRef } from '@angular/core'
 })
 export class ContainerInContainerComponent implements OnInit {
 
-  @ViewChild('test') editor!: ElementRef;
-
+  @ViewChild('blueEditor') editor!: ElementRef;
+  isGoodContainer : boolean = false;
 
 
 
@@ -28,10 +28,32 @@ export class ContainerInContainerComponent implements OnInit {
     const image : HTMLImageElement = this.renderer.createElement('img');
     image.src = 'https://64.media.tumblr.com/763756ea907e30b639da239618bbe2d3/tumblr_mlotjw0e2C1r4xjo2o1_250.gifv';
     this.renderer.appendChild(this.editor.nativeElement,image);
-    
+    this.isGoodContainer =  false; 
   }
 
+  onDragEntered(event:CdkDragEnter){
+   // console.log(event.container.element.nativeElement.id);
+   // console.log(this.editor.nativeElement.id);
+    if(this.editor.nativeElement.id == event.container.element.nativeElement.id){
+      this.isGoodContainer = true;
+      
+    }   
+  }
 
+  addContainer_2_blocs(event: CdkDragDrop<string[]>){
+    const div : HTMLDivElement = this.renderer.createElement('div');
+    const div1 : HTMLDivElement = this.renderer.createElement('div');
+    const div2 : HTMLDivElement = this.renderer.createElement('div');
+    this.renderer.appendChild(div,div1);
+    this.renderer.appendChild(div,div2);
+    this.renderer.addClass(div,'temp');
+
+
+
+
+
+    this.renderer.appendChild(this.editor.nativeElement,div);
+  }
 
 
 
